aa234340e260038616dc5d07849da636a18dcf12
Richard Goulter <richard.goulter@gmail.com>
2020-08-26 13:45:43 +0700

    home-row mod on dvorak

--- layouts/community/planck_mit/rgoulter/keymap.c
+++ layouts/community/planck_mit/rgoulter/keymap.c
@@ -37,6 +37,18 @@ enum custom_keycodes {
 #define RCTLESC RCTL_T(KC_ESC)
 #define RCTLENT RCTL_T(KC_ENT)
 
+/* Home Row Mods, Dvorak, LH */
+#define LGUIT_A LGUI_T(KC_A)
+#define LALTT_O LALT_T(KC_O)
+#define LCTLT_E LCTL_T(KC_E)
+#define LSFTT_U LSFT_T(KC_U)
+
+/* Home Row Mods, Dvorak, RH */
+#define RSFTT_H RSFT_T(KC_H)
+#define RCTLT_T RCTL_T(KC_T)
+#define RALTT_N RALT_T(KC_N)
+#define RGUIT_S RGUI_T(KC_S)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_mit( \
@@ -55,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_DVORAK] = LAYOUT_planck_mit( \
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
-  LCTLESC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    RCTLENT, \
+  LCTLESC, LGUIT_A, LALTT_O, LCTLT_E, LSFTT_U, KC_I,    KC_D,    RSFTT_H, RCTLT_T, RALTT_N, RGUIT_S, RCTLENT, \
   KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, \
   ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,      KC_SPC,        RAISE,   KC_RGUI, KC_RALT, KC_RCTL, NUMPAD \
 ),

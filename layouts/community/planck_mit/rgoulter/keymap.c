f3ce90bd9955cb799a7eb538a27d67a97d108182
Richard Goulter <richard.goulter@gmail.com>
2020-11-10 13:52:22 +0700

    Revert "allow enabling _ADJUST on its own"
    This reverts commit 16236939408fe2aeb74d597efa31e5a8a900f965.

--- layouts/community/planck_mit/rgoulter/keymap.c
+++ layouts/community/planck_mit/rgoulter/keymap.c
@@ -158,14 +158,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  if (layer_state_cmp(state, _ADJUST)) {
-    // allow enabling _ADJUST other than just with _LOWER and _RAISE
-    return state;
-  } else {
-    // if both _LOWER and _RAISE layer are enabled,
-    // also enable the _ADJUST layer
-    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-  }
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {

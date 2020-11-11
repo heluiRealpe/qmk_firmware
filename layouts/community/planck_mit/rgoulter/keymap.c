c0380662d579ae4d4597753cc3240ed7eb0d7ac6
Richard Goulter <richard.goulter@gmail.com>
2020-11-11 10:26:09 +0700

    Simplify layout: remove the stuff I'm not using

--- layouts/community/planck_mit/rgoulter/keymap.c
+++ layouts/community/planck_mit/rgoulter/keymap.c
@@ -13,7 +13,6 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 enum layers {
   _QWERTY,
-  _COLEMAK,
   _DVORAK,
   _SNAKE,
   _LOWER,
@@ -37,7 +36,6 @@ char quarter_count = 0;
 char half_count = 0;
 
 #define QWERTY     DF(_QWERTY)
-#define COLEMAK    DF(_COLEMAK)
 #define DVORAK     DF(_DVORAK)
 #define CHILDPROOF DF(_CHILDPROOF)
 #define LOWER   MO(_LOWER)
@@ -45,71 +43,48 @@ char half_count = 0;
 #define NUMPAD  MO(_NUMPAD)
 #define ADJUST  MO(_ADJUST)
 
-#define GUIBSPC GUI_T(KC_BSPC)
-#define RSFTENT RSFT_T(KC_ENT)
-#define LSFTENT LSFT_T(KC_ENT)
+/* Home Row, outer column */
 #define LCTLESC LCTL_T(KC_ESC)
-#define RCTLESC RCTL_T(KC_ESC)
 #define RCTLENT RCTL_T(KC_ENT)
 
 /* Home Row Mods, Dvorak, LH */
-#define LGUIT_A LGUI_T(KC_A)
-#define LALTT_O LALT_T(KC_O)
+#define LALTT_A LALT_T(KC_A)
+#define LGUIT_O LGUI_T(KC_O)
 #define LCTLT_E LCTL_T(KC_E)
 #define LSFTT_U LSFT_T(KC_U)
 
 /* Home Row Mods, QWERTY, LH */
+// #define LALTT_A LALT_T(KC_A)
+#define LGUIT_S LGUI_T(KC_S)
+#define LCTLT_D LCTL_T(KC_D)
 #define LSFTT_F LSFT_T(KC_F)
-
-/* Lower Row Mods, Dvorak, LH */
-#define LGUIT_Q LGUI_T(KC_Q)
-#define LALTT_J LALT_T(KC_J)
-#define LCTLT_K LCTL_T(KC_K)
-
-/* Lower Row Mods, QWERTY, LH */
-#define LGUIT_X LGUI_T(KC_X)
-#define LALTT_C LALT_T(KC_C)
-#define LCTLT_V LCTL_T(KC_V)
 
 /* Home Row Mods, Dvorak, RH */
 #define RSFTT_H RSFT_T(KC_H)
 #define RCTLT_T RCTL_T(KC_T)
-#define RALTT_N RALT_T(KC_N)
-#define RGUIT_S RGUI_T(KC_S)
+#define RGUIT_N RGUI_T(KC_N)
+#define RALTT_S RALT_T(KC_S)
 
 /* Home Row Mods, QWERTY, RH */
 #define RSFTT_J RSFT_T(KC_J)
-
-/* Lower Row Mods, Dvorak, RH */
-#define RCTLT_M RCTL_T(KC_M)
-#define RALTT_W RALT_T(KC_W)
-#define RGUIT_V RGUI_T(KC_V)
-
-/* Lower Row Mods, QWERTY, RH */
-#define RA_COMM RALT_T(KC_COMM)
-#define RG_DOT  RGUI_T(KC_DOT)
+#define RCTLT_K RCTL_T(KC_K)
+#define RGUIT_L RGUI_T(KC_L)
+#define RALTTSC RALT_T(KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_mit( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  LCTLESC, KC_A,    KC_S,    KC_D,    LSFTT_F, KC_G,    KC_H,    RSFTT_J, KC_K,    KC_L,    KC_SCLN, RCTLENT, \
-  KC_LSFT, KC_Z,    LGUIT_X, LALTT_C, LCTLT_V, KC_B,    KC_N,    RCTLT_M, RA_COMM, RG_DOT,  KC_QUOT, KC_RSFT, \
-  ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,      KC_SPC,        RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
-),
-
-[_COLEMAK] = LAYOUT_planck_mit( \
-  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
-  KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,      KC_SPC,        RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  LCTLESC, LALTT_A, LGUIT_S, LCTLT_D, LSFTT_F, KC_G,    KC_H,    RSFTT_J, RCTLT_K, RGUIT_L, RALTTSC, RCTLENT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, KC_RSFT, \
+  _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,      KC_SPC,        RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 [_DVORAK] = LAYOUT_planck_mit( \
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
-  LCTLESC, LGUIT_A, LALTT_O, LCTLT_E, LSFTT_U, KC_I,    KC_D,    RSFTT_H, RCTLT_T, RALTT_N, RGUIT_S, RCTLENT, \
+  LCTLESC, LALTT_A, LGUIT_O, LCTLT_E, LSFTT_U, KC_I,    KC_D,    RSFTT_H, RCTLT_T, RGUIT_N, RALTT_S, RCTLENT, \
   KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, \
-  ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,      KC_SPC,        RAISE,   KC_RGUI, KC_RALT, KC_RCTL, NUMPAD \
+  _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,      KC_SPC,        RAISE,   KC_RGUI, KC_RALT, KC_RCTL, NUMPAD \
 ),
 
 [_SNAKE] = LAYOUT_planck_mit( \
@@ -137,19 +112,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  ADJUST,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+  LOWER,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RAISE \
 ),
 
 [_NUMPAD] = LAYOUT_planck_mit( \
-  _______, _______, _______, KC_UP,   _______, KC_INS,  KC_HOME, KC_PGUP, KC_7,    KC_8,    KC_9,    _______, \
-  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  KC_END,  KC_PGDN, KC_4,    KC_5,    KC_6,    _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,    _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    _______, \
   _______, _______, _______, _______, _______,     _______,      _______, KC_0,    KC_0,    KC_DOT,  _______  \
 ),
 
 [_ADJUST] =  LAYOUT_planck_mit( \
   RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, _______,
-  _______, _______, _______, _______, _______, AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  CHILDPROOF, SNAKE, \
+  _______, _______, _______, _______, _______, _______, _______, QWERTY,  XXXXXXX, DVORAK,  CHILDPROOF, SNAKE, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, KC_WH_D, KC_WH_U, \
   _______, _______, _______, _______, _______,     _______,      _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R \
 )

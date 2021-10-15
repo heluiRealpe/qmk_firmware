20d73f2c0a2ccdabdbb934d7f31623ff4e17dd06
Richard Goulter <richard.goulter@gmail.com>
2021-10-15 18:50:15 +0700

    add segments

--- users/rgoulter/rgoulter.h
+++ users/rgoulter/rgoulter.h
@@ -33,6 +33,43 @@
 #define RGUIT_L RGUI_T(KC_L)
 #define RALTTSC RALT_T(KC_SCLN)
 
+#define ___SEG3_123___ KC_1, KC_2, KC_3
+#define ___SEG3_456___ KC_4, KC_5, KC_6
+#define ___SEG3_789___ KC_7, KC_8, KC_9
+
+#define ___SEG3_S123___ KC_EXLM, KC_AT, KC_HASH
+#define ___SEG3_S456___ KC_DLR,  KC_PERC, KC_CIRC
+#define ___SEG3_S789___ KC_AMPR, KC_ASTR, KC_LPRN
+
+#define ___SEG3_F123___ KC_F1, KC_F2, KC_F3
+#define ___SEG3_F456___ KC_F4, KC_F5, KC_F6
+#define ___SEG3_F789___ KC_F7, KC_F8, KC_F9
+
+#define ___SEG4_NAV2___ KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+#define ___SEG4_NAV3___ KC_HOME, KC_PGDN, KC_PGUP, KC_END
+
+#define ___SEG4_MOU_MV___ KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R
+#define ___SEG4_MOU_WH___ KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R
+#define ___SEG3_MOU_BTN___ KC_BTN1, KC_BTN2, KC_BTN3
+
+#define ___SEG4_MED___ KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+
+#define ___SEG5_DVORAK_LHS_1___ KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y
+#define ___SEG5_DVORAK_LHS_2___ LALTT_A, LGUIT_O, LCTLT_E, LSFTT_U, KC_I
+#define ___SEG5_DVORAK_LHS_3___ KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X
+
+#define ___SEG5_DVORAK_RHS_1___ KC_F,    KC_G,    KC_C,    KC_R,    KC_L
+#define ___SEG5_DVORAK_RHS_2___ KC_D,    RSFTT_H, RCTLT_T, RGUIT_N, RALTT_S
+#define ___SEG5_DVORAK_RHS_3___ KC_B,    KC_M,    KC_W,    KC_V,    KC_Z
+
+#define ___SEG5_QWERTY_LHS_1___ KC_Q,    KC_W,    KC_E,    KC_R,    KC_T
+#define ___SEG5_QWERTY_LHS_2___ LALTT_A, LGUIT_S, LCTLT_D, LSFTT_F, KC_G
+#define ___SEG5_QWERTY_LHS_3___ KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
+
+#define ___SEG5_QWERTY_RHS_1___ KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
+#define ___SEG5_QWERTY_RHS_2___ KC_H,    RSFTT_J, RCTLT_K, RGUIT_L, RALTTSC
+#define ___SEG5_QWERTY_RHS_3___ KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT
+
 #define ___BASE_BOTTOM_ROW___ \
   _______, _______, _______, LWR_TAB, LW2_ESC,    KC_SPC,      RS2_BSP,    RSE_ENT, _______, _______, _______
 

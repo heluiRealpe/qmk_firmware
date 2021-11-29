6937cf1c59cc9161833b83708d2973a225b5fa23
Richard Goulter <richard.goulter@gmail.com>
2021-11-29 09:33:14 +0700

    use develop's shell.nix; my rebase had overwritten it.

--- shell.nix
+++ shell.nix
@@ -38,7 +38,8 @@ let
 in
 mkShell {
   name = "qmk-firmware";
-  buildInputs = [ clang-tools dfu-programmer dfu-util diffutils hidapi git pythonEnv poetry niv ]
+
+  buildInputs = [ clang-tools dfu-programmer dfu-util diffutils git pythonEnv poetry niv ]
     ++ lib.optional avr [
       pkgsCross.avr.buildPackages.binutils
       pkgsCross.avr.buildPackages.gcc8
@@ -54,6 +55,5 @@ mkShell {
     # Prevent the avr-gcc wrapper from picking up host GCC flags
     # like -iframework, which is problematic on Darwin
     unset NIX_CFLAGS_COMPILE_FOR_TARGET
-    export LD_LIBRARY_PATH=${hidapi}/lib:$LD_LIBRARY_PATH
   '';
 }

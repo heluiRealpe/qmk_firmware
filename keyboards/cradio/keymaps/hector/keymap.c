3fe4c0261f8e710421473afbffe6e6da08badde9
heluiRealpe <hrealpe@gmail.com>
2023-05-17 13:54:22 +0200

    cradio updated

--- keyboards/cradio/keymaps/hector/keymap.c
+++ keyboards/cradio/keymaps/hector/keymap.c
@@ -117,7 +117,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
     LSHIFT_Z, LCTRL_X, LALT_C , LGUI_V ,  KC_B  ,                      KC_ENE , RGUIL_M,RALT_COM,RCTR_DOT,RSHIFT_H,
   //--------+--------+--------+--------|--------+--------|  |--------+--------+--------+--------+--------+--------|
--
                                           LOWER , SPCNUM ,    KC_SPC , RAISE
                                       //`----------------'  `----------------'
   ),
@@ -247,11 +246,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
   [L_MOV] = LAYOUT_split_3x5_2(
   //,--------------------------------------------.                    ,--------------------------------------------.
-      _______, KC_ACL0, KC_ACL1, KC_ACL2, _______,                      KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, _______,
+      QWERTY , KC_ACL0, KC_ACL1, KC_ACL2, QWERTY ,                      KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, QWERTY ,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-      KC_ESC , QWERTY , KC_BTN2, KC_BTN1, _______,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_SLEP,
+      KC_ESC , QWERTY , KC_BTN2, KC_BTN1, QWERTY ,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_SLEP,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-      KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,                      KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,
+      KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, QWERTY ,                      KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, QWERTY ,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
                                           _______, _______,    _______, _______
                                       //`-----------------'  `-----------------'
